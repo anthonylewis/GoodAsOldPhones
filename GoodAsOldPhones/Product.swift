12ef2944e9d9895ebaf262c22ea1b79d30b1d168
Anthony Lewis <me@anthonylewis.com>
2016-04-19 22:38:56 -0500

    Add and use Product class

---
+++ GoodAsOldPhones/Product.swift
@@ -0,0 +1,15 @@
+//
+//  Product.swift
+//  GoodAsOldPhones
+//
+//  Created by Anthony Lewis on 4/19/16.
+//  Copyright © 2016 Anthony Lewis. All rights reserved.
+//
+
+import Foundation
+
+class Product {
+    var name: String?
+    var productImage: String?
+    var cellImage: String?
+}
